eaddac411f48b702613861d665afa173e966faee
Suraj C K <surajck4@gmail.com>
2023-07-29 18:11:10 +0530

    Calendar view and styling

--- src/components/GoalScreen.tsx
+++ src/components/GoalScreen.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {Dimensions} from 'react-native';
-import {useSwipe} from '../utils/useSwipe';
+import {useSwipe, getDateArray} from '../utils/useSwipe';
 import styleFactory from './GoalScreenStyles';
+
+interface DayViewProps {
+  date: any;
+  ones: Array<number>;
+  styles: any;
+}
+function DayView(props: DayViewProps) {
+  const dateObj = getDateArray(props.date);
+
+  console.log(dateObj);
+
+  return (
+    <View style={props.styles.calendarColumn}>
+      <View style={props.styles.dateContainer}>
+        <Text style={props.styles.dateField}>{dateObj.month}</Text>
+        <Text style={props.styles.dateNum}>{dateObj.date}</Text>
+        <Text style={props.styles.dateField}>{dateObj.weekday}</Text>
+      </View>
+      <View style={props.styles.onesColumn}>
+        {props.ones.map(one => {
+          const oneStyle = one
+            ? props.styles.activeOne
+            : props.styles.passiveOne;
+          return <Text style={oneStyle}>1</Text>;
+        })}
+      </View>
+    </View>
+  );
+}
 
 function GoalScreen() {
   const windowWidth = Dimensions.get('window').width;
@@ -18,6 +47,30 @@ function GoalScreen() {
   }
 
   const {onTouchStart, onTouchEnd} = useSwipe(onSwipeLeft, onSwipeRight, 6);
+
+  let onesData = [
+    {date: new Date(), ones: [1, 0, 1, 1, 0]},
+    {date: new Date('7/27/23'), ones: [1, 1, 1, 0, 1]},
+    {date: new Date('7/26/23'), ones: [1, 0, 0, 1, 0]},
+    {date: new Date('7/25/23'), ones: [1, 1, 0, 0, 1]},
+    {date: new Date('7/24/23'), ones: [0, 0, 1, 1, 0]},
+    {date: new Date('7/23/23'), ones: [1, 0, 1, 1, 1]},
+    {date: new Date('7/22/23'), ones: [1, 0, 1, 1, 1]},
+    {date: new Date('7/21/23'), ones: [0, 1, 1, 1, 0]},
+    {date: new Date('7/20/23'), ones: [1, 1, 1, 1, 0]},
+    {date: new Date('7/19/23'), ones: [1, 0, 1, 1, 1]},
+  ];
+
+  const calendarItem: Array<any> = [];
+  onesData.forEach(onesDataRow => {
+    calendarItem.push(
+      <DayView
+        date={onesDataRow.date}
+        ones={onesDataRow.ones}
+        styles={styles}
+      />,
+    );
+  });
 
   return (
     <View
@@ -41,13 +94,7 @@ function GoalScreen() {
           <Text style={styles.taskItem}>May the 4th be with you</Text>
           <Text style={styles.taskItem}>Cinco de Mayo</Text>
         </View>
-        <View style={styles.calendarContainer}>
-          <Text style={styles.taskItem}>Task number one</Text>
-          <Text style={styles.taskItem}>Two times lucky</Text>
-          <Text style={styles.taskItem}>Fool me thrice, strike one</Text>
-          <Text style={styles.taskItem}>May the 4th be with you</Text>
-          <Text style={styles.taskItem}>Cinco de Mayo</Text>
-        </View>
+        <View style={styles.calendarContainer}>{calendarItem}</View>
       </View>
     </View>
   );
--- src/components/GoalScreenStyles.ts
+++ src/components/GoalScreenStyles.ts
@@ -3,6 +3,7 @@ import {StyleSheet} from 'react-native';
 export default (wW: number, wH: number) => {
   return StyleSheet.create({
     mainContainer: {},
+
     goalHeaderContainer: {
       marginTop: 70,
       marginBottom: 30,
@@ -12,34 +13,37 @@ export default (wW: number, wH: number) => {
       justifyContent: 'space-between',
       alignItems: 'center',
     },
+
     goalNameContainer: {},
+
     goalName: {
       color: '#B74F6F',
       fontSize: 30,
       fontWeight: 'bold',
     },
+
     goalCountContainer: {
       flexDirection: 'row',
     },
+
     goalCountDone: {
       color: '#B74F6F',
       fontSize: 20,
       fontWeight: 'bold',
     },
+
     goalCountTarget: {
       color: '#3A5068',
       fontSize: 20,
       fontWeight: 'bold',
     },
+
     tasksAndCalendarContainer: {
       flexDirection: 'row-reverse',
       position: 'relative',
       right: -0.9 * wW,
     },
-    calendarContainer: {
-      flexBasis: 0.9 * wW,
-      backgroundColor: 'red',
-    },
+
     tasksContainer: {
       flexBasis: wW,
       paddingTop: 70,
@@ -47,9 +51,53 @@ export default (wW: number, wH: number) => {
       paddingLeft: 35,
       paddingRight: 35,
     },
+
     taskItem: {
       color: '#3A5068',
       fontSize: 24,
+      textAlign: 'center',
+      marginTop: 25,
+      marginBottom: 25,
+    },
+
+    calendarContainer: {
+      flexBasis: 0.9 * wW,
+      flexDirection: 'row-reverse',
+    },
+
+    calendarColumn: {
+      width: 70,
+      textAlign: 'center',
+    },
+
+    dateContainer: {},
+
+    dateNum: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      color: '#B74F6F',
+    },
+
+    dateField: {
+      textAlign: 'center',
+      color: '#355863',
+    },
+
+    onesColumn: {},
+
+    activeOne: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#6FFFE9',
+      textAlign: 'center',
+      marginTop: 25,
+      marginBottom: 25,
+    },
+
+    passiveOne: {
+      fontSize: 20,
+      color: '#355863',
       textAlign: 'center',
       marginTop: 25,
       marginBottom: 25,
